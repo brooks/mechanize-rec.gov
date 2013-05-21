f66302f36c9dc20eea12068099cf781cd3986635
Broppler <brooksmason@gmail.com>
2013-05-20 22:28:42 -0700

    finished back-end

--- recreation.rb
+++ recreation.rb
@@ -10,13 +10,25 @@ class Bot
     @agent.get(CAMPGROUND_URL)
   end
 
+  def change_value(field_num, new_value)
+    @agent.page.forms[0].fields[field_num].value = new_value
+  end
+
+  def submit_form(num)
+    @agent.page.forms[num].submit
+  end
+
+  def check_box(name)
+    @agent.page.forms[0].checkbox_with(:name => name).check
+  end
+
   def enter_search_criteria
-    @agent.page.forms[0].fields[6].value = SITES
-    @agent.page.forms[0].fields[20].value = ARRIVAL
-    @agent.page.forms[0].fields[21].value = NIGHTS
-    @agent.page.forms[0].fields[23].value = '2003'
-    @agent.page.forms[0].fields[9].value = 6   
-    @agent.page.forms[0].submit
+    change_value(6, SITES)
+    change_value(20, ARRIVAL)
+    change_value(21, NIGHTS)
+    change_value(23, '2003') # '2003' => tent
+    change_value(9, 6)       # 6 => max occupants
+    submit_form(0)
   end
 
   def click_first_available_site
@@ -24,37 +36,47 @@ class Bot
   end
 
   def click_book_these_dates
-    @agent.page.forms[0].submit
+    submit_form(0)
   end
 
   def enter_login_credentials
-    @agent.page.forms[0].fields[0].value = EMAIL
-    @agent.page.forms[0].fields[1].value = PASSWORD
-    @agent.page.forms[0].submit
+    change_value(0, EMAIL)
+    change_value(1, PASSWORD)
+    submit_form(0)
   end
 
   def enter_site_details
-    @agent.page.forms[0].fields[19].value = 108060
-    @agent.page.forms[0].fields[4].value = 6
-    @agent.page.forms[0].fields[5].value = 2
-    @agent.page.forms[0].checkbox_with(:name => 'agreementAccepted').check
-    @agent.page.forms[0].submit
+    change_value(19, 108060) # 108060 => tent
+    change_value(4, 6)       # 6 => max occupants
+    change_value(5, 2)       # 3 => max vehicles
+    check_box('agreementAccepted')
+    submit_form(0)
   end
 
   def proceed_to_checkout
-    @agent.page.forms[1].submit
+    submit_form(1)
   end
 
   def enter_billing_information
-    @agent.page.forms[0].fields[20].value = CARD_TYPE
-    @agent.page.forms[0].fields[5].value = CARD_NO
-    @agent.page.forms[0].fields[6].value = SECURITY_CODE
-    @agent.page.forms[0].fields[7].value = EXPIRATION_MONTH
-    @agent.page.forms[0].fields[8].value = EXPIRATION_YEAR
-    @agent.page.forms[0].fields[9].value = FIRST_NAME
-    @agent.page.forms[0].fields[10].value = LAST_NAME
-    @agent.page.forms[0].checkbox_with(:name => 'acknowlegeAccepted').check
-    @agent.page.forms[0].submit
+    change_value(20, CARD_TYPE)
+    change_value(5, CARD_NO)
+    change_value(6, SECURITY_CODE)
+    change_value(7, EXPIRATION_MONTH)
+    change_value(8, EXPIRATION_YEAR)
+    change_value(9, FIRST_NAME)
+    change_value(10, LAST_NAME)
+    check_box('acknowlegeAccepted')
+    submit_form(0)
+  end
+
+  def fetch_site!
+    self.enter_search_criteria
+    self.click_first_available_site
+    self.click_book_these_dates
+    self.enter_login_credentials
+    self.enter_site_details
+    self.proceed_to_checkout
+    # self.enter_billing_information
   end
 
   def print_links
@@ -75,12 +97,5 @@ class Bot
 end
 
 cyber_raccoon = Bot.new
-cyber_raccoon.enter_search_criteria
-cyber_raccoon.print_forms
-cyber_raccoon.click_first_available_site
-cyber_raccoon.click_book_these_dates
-cyber_raccoon.enter_login_credentials
-cyber_raccoon.enter_site_details
-cyber_raccoon.proceed_to_checkout
-
+cyber_raccoon.fetch_site!
 cyber_raccoon.print_forms
