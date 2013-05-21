4c9cca2532a70d13878fe9155d566025d3c818dd
Broppler <brooksmason@gmail.com>
2013-05-20 21:19:51 -0700

    refactored, added tent choice on search screen

--- recreation.rb
+++ recreation.rb
@@ -3,56 +3,84 @@ require 'mechanize'
 require 'open-uri'
 require_relative 'private/access'
 
-puts CARD_NO
+class Bot
 
-# agent = Mechanize.new
+  def initialize
+    @agent = Mechanize.new
+    @agent.get(CAMPGROUND_URL)
+  end
 
-# agent.get('http://www.recreation.gov/camping/North_Pines/r/campgroundDetails.do?contractCode=NRSO&parkId=70927&topTabIndex=Search')
+  def enter_search_criteria
+    @agent.page.forms[0].fields[6].value = SITES
+    @agent.page.forms[0].fields[20].value = ARRIVAL
+    @agent.page.forms[0].fields[21].value = NIGHTS
+    @agent.page.forms[0].fields[23].value = '2003'
+    @agent.page.forms[0].fields[9].value = 6   
+    @agent.page.forms[0].submit
+  end
 
-# agent.page.forms[0].fields.each_with_index do |field, index|
-#   puts "#{index}: #{field.name}"
-# end
+  def click_first_available_site
+    @agent.page.link_with(:dom_class => 'book now').click
+  end
 
-# agent.page.forms[0].fields[6].value = SITES
-# agent.page.forms[0].fields[20].value = ARRIVAL
-# agent.page.forms[0].fields[21].value = NIGHTS
-# agent.page.forms[0].submit
+  def click_book_these_dates
+    @agent.page.forms[0].submit
+  end
 
-# agent.click(agent.page.link_with(:class => 'book now'))
+  def enter_login_credentials
+    @agent.page.forms[0].fields[0].value = EMAIL
+    @agent.page.forms[0].fields[1].value = PASSWORD
+    @agent.page.forms[0].submit
+  end
 
-# agent.page.forms[0].submit
+  def enter_site_details
+    @agent.page.forms[0].fields[19].value = 108060
+    @agent.page.forms[0].fields[4].value = 6
+    @agent.page.forms[0].fields[5].value = 2
+    @agent.page.forms[0].checkbox_with(:name => 'agreementAccepted').check
+    @agent.page.forms[0].submit
+  end
 
-# agent.page.forms[0].fields[0].value = USERNAME
-# agent.page.forms[0].fields[1].value = PASSWORD
-# agent.page.forms[0].submit
+  def proceed_to_checkout
+    @agent.page.forms[1].submit
+  end
 
-# agent.page.forms[0].fields[19].value = 108060
-# agent.page.forms[0].fields[4].value = 6
-# agent.page.forms[0].fields[5].value = 2
-# agent.page.forms[0].checkbox_with(:name => 'agreementAccepted').check
-# agent.page.forms[0].submit
+  def enter_billing_information
+    @agent.page.forms[0].fields[20].value = CARD_TYPE
+    @agent.page.forms[0].fields[5].value = CARD_NO
+    @agent.page.forms[0].fields[6].value = SECURITY_CODE
+    @agent.page.forms[0].fields[7].value = EXPIRATION_MONTH
+    @agent.page.forms[0].fields[8].value = EXPIRATION_YEAR
+    @agent.page.forms[0].fields[9].value = FIRST_NAME
+    @agent.page.forms[0].fields[10].value = LAST_NAME
+    @agent.page.forms[0].checkbox_with(:name => 'acknowlegeAccepted').check
+    @agent.page.forms[0].submit
+  end
 
-# agent.page.forms[1].submit
+  def print_links
+    @agent.page.links.each do |link|
+      puts link.href
+    end
+  end
 
-# agent.page.forms[0].fields[20].value = CARD_TYPE
-# agent.page.forms[0].fields[5].value = CARD_NO
-# agent.page.forms[0].fields[6].value = SECURITY_CODE
-# agent.page.forms[0].fields[7].value = EXPIRATION_MONTH
-# agent.page.forms[0].fields[8].value = EXPIRATION_YEAR
-# agent.page.forms[0].fields[9].value = FIRST_NAME
-# agent.page.forms[0].fields[10].value = LAST_NAME
+  def print_forms
+    @agent.page.forms[0].fields.each_with_index do |field, index|
+      puts "#{index}: #{field.name}"
+    end
+  end
 
+  def print_body
+    puts @agent.page.body
+  end
+end
 
+cyber_raccoon = Bot.new
+cyber_raccoon.enter_search_criteria
+cyber_raccoon.print_forms
+cyber_raccoon.click_first_available_site
+cyber_raccoon.click_book_these_dates
+cyber_raccoon.enter_login_credentials
+cyber_raccoon.enter_site_details
+cyber_raccoon.proceed_to_checkout
 
-# agent.page.forms[0].checkbox_with(:name => 'acknowlegeAccepted').check
-# agent.page.forms[0].submit
-
-
-# agent.page.links.each do |link|
-#   puts link.href
-# end
-
-# agent.page.forms[0].fields.each_with_index do |field, index|
-#   puts "#{index}: #{field.name}"
-# end
-
+cyber_raccoon.print_forms
