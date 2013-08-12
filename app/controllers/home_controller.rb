5411adc8e38732302fc4b046db21586c162e2bb1
Broppler <brooksmason@gmail.com>
2013-08-12 00:37:11 -0700

    ripped devise; added basic login structure to home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,34 @@
 class HomeController < ApplicationController
 
-before_filter :authenticate_user!
+  before_filter :check_login_status, :except => [:login, :verify, :register]
+
+  def login
+  end
+
+  def verify
+    user = User.find_by email: params[:email]
+    if user.nil?
+      redirect_to login_path, flash: { error: "nicetry" }
+    elsif user.password == params[:password]
+      @current_user = user
+      redirect_to home_path, notice: "Welcome"
+    else
+      redirect_to login_path, flash: { error: "comeon" }
+    end
+  end
+
+  def register
+    user = User.create(email: params[:email], password: params[:password])
+    @current_user = user
+    redirect_to home_path, notice: "Welcome"
+  end
 
   def index
+
+  end
+
+  def check_login_status
+    redirect_to login_path if @current_user.nil?
   end
 
 end
